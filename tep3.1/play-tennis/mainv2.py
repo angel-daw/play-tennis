c03ec52600fc4b7b73c4684de05ad7d84c7c87d1
gargonjj <jesuramone@gmail.com>
2025-11-18 12:08:15 +0000

    Actualizar mainv2.py

--- tep3.1/play-tennis/mainv2.py
+++ tep3.1/play-tennis/mainv2.py
@@ -1,65 +1,44 @@
 def run(points: str) -> str:
-    points_a = 0
-    points_b = 0
-    a_sets = 0
-    b_sets = 0
+    a_points = 0
+    b_points = 0
     a_games = 0
     b_games = 0
-    TIE_BREAK_CONDITION = points_a == 6 and points_b == points_a
-    # Creo que tiene más sentido plantear la condición en la constante directamente
-    # y que el bucle comprueba si se cumple o no, en lugar de cambiar su valor
-    # entre True y False
+    a_sets = 0
+    b_sets = 0
+    tiebreak_condition = False
     result = ''
     for point in points:
+        if a_games == 6 and b_games == 6:
+            tiebreak = True
         if point == 'A':
             points_a += 1
         else:
             points_b += 1
         # COMPROBACIÓN DE EMPATE
-        if TIE_BREAK_CONDITION:
-            if points_a >= 7 and points_a - points_b >= 2:
-                a_games += 1
-                result += f'{points_a} - {points_b} '
-                a_sets = 0
-                b_sets = 0
-                points_a = 0
-                points_b = 0
-            if points_b >= 7 and points_b - points_a >= 2:
-                b_games += 1
-                result += f'{points_a} - {points_b} '
-                a_sets = 0
-                b_sets = 0
-                points_a = 0
-                points_b = 0
         # COMPROBACIÓN DE SET PARA A
-        if points_a >= 4 and points_a - points_b >= 2:
-            a_sets += 1
-            points_a = 0
-            points_b = 0
-        # COMPROBACIÓN DE SET PARA B
-        elif points_b >= 4 and points_b - points_a >= 2:
-            b_sets += 1
-            points_b = 0
-            points_a = 0
-        # COMPROBACIÓN DE JUEGO PARA A
-        if a_sets >= 6 and a_sets - b_sets >= 2:
+        if a_points >= 4 and a_points - b_points >= 2:
             a_games += 1
-            result += f'{a_sets}-{b_sets} '
-            a_sets = 0
-            b_sets = 0
-        # COMPROBACIÓN DE JUEGO PARA B
-        elif b_sets >= 6 and b_sets - a_sets >= 2:
+            a_points = 0
+            b_points = 0
+        # COMPROBACIÓN DE SET PARA B
+        elif b_points >= 4 and b_points - a_points >= 2:
             b_games += 1
-            result += f'{a_sets}-{b_sets} '
-            b_sets = 0
-            a_sets = 0
-        if b_sets > 7 and a_sets > 7:
-            b_sets = 0
-            a_sets = 0
-            points_a = 0
-            points_b = 0
+            a_points = 0
+            b_points = 0
+        # COMPROBACIÓN DE JUEGO PARA A
+        if a_games >= 6 and a_games - b_games >= 2:
+            a_sets += 1
+            result += f'{a_games}-{b_games} '
+            a_games = 0
+            b_games = 0
+        # COMPROBACIÓN DE JUEGO PARA B
+        elif b_games >= 6 and b_games - a_games >= 2:
+            b_sets += 1
+            result += f'{a_games}-{b_games} '
+            b_games = 0
+            a_games = 0
         # COMPROBACIÓN DE PARTIDO
-        if a_games == 2 or b_games == 2:
+        if a_sets == 2 or b_sets == 2:
             break
     return result
 
